78610e1b6e3b0c1edf741dfcae21223d4101f073
Dalila <dalila.herh@gmail.com>
2019-04-08 10:50:35 +0200

    drugi

---
+++ src/ba/unsa/etf/rs/tut5/Korisnik.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rs.tut5;
+
+import javafx.beans.property.SimpleSetProperty;
+
+public class Korisnik {
+    private SimpleSetProperty ime, prezime, email, username, password;
+    
+}
